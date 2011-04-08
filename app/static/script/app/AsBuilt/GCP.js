ca9ada658547cf2f13196846d8f5c7b56dd2042a
Bart van den Eijnden <bartvde@opengeo.org>
2011-04-08 16:31:19 +0200

    let the GCP tool create the vector layer instead

--- app/static/script/app/AsBuilt/GCP.js
+++ app/static/script/app/AsBuilt/GCP.js
@@ -5,51 +5,33 @@ AsBuilt.GCP = Ext.extend(gxp.plugins.Tool, {
     /** api: ptype = app_gcp */
     ptype: "app_gcp",
 
-    /** api: method[activate]
-     *  :returns: ``Boolean`` true when this tool was activated
-     *
-     *  Activates this tool. When active, this tool loads the features for the
-     *  configured layer, or listens to layer changes on the application and
-     *  loads features for the selected layer.
-     */
-    activate: function() {
-        if (AsBuilt.GCP.superclass.activate.apply(this, arguments)) {
-            if (this.layer) {
-                this.target.createLayerRecord(this.layer, this.setLayer, this);
-            }
-            return true;
-        }
-    },
-
-    setLayer: function(layerRecord) {
-        this.layerRecord = layerRecord;
-        var layer = this.layerRecord.get("layer");
-        this.target.mapPanel.map.addLayer(layer);
-        this.drawControl.layer = layer;
-    },
-
     /** api: method[addActions]
      */
     addActions: function() {
+        this.layer = new OpenLayers.Layer.Vector();
         this.drawControl = new OpenLayers.Control.DrawFeature(
-            new OpenLayers.Layer.Vector(),    
+            this.layer,    
             OpenLayers.Handler.Point, {
                 eventListeners: {
                     featureadded: function(evt) {
+                        if (!this.layer.map) {
+                            this.target.mapPanel.map.addLayer(this.layer);
+                        }
                         var geometry = evt.feature.geometry;
                         this.drawControl.deactivate();
                         if (this.coords === "image") {
-                            var pixel = new OpenLayers.Pixel(geometry.x, geometry.y);
-                            this.gcps.push({pixel: pixel});
+                            this.gcps.push({pixel: geometry});
                         } else if (this.coords === "world") {
-                            var lonlat = new OpenLayers.LonLat(geometry.x, geometry.y);
-                            this.gcps[this.gcps.length-1].lonlat = lonlat;
+                            this.gcps[this.gcps.length-1].lonlat = geometry;
                         }
                     },
                     scope: this
                 }
             }
         );
+        this.modifyControl = new OpenLayers.Control.ModifyFeature(
+            this.layer
+        );    
         var toggleGroup = this.toggleGroup || Ext.id();
         var actions = AsBuilt.GCP.superclass.addActions.call(this, [new GeoExt.Action({
             tooltip: "Draw GCP",
@@ -58,6 +40,15 @@ AsBuilt.GCP = Ext.extend(gxp.plugins.Tool, {
             enableToggle: true,
             allowDepress: true,
             control: this.drawControl,
+            deactivateOnDisable: true,
+            map: this.target.mapPanel.map
+        }), new GeoExt.Action({
+            tooltip: "Edit GCP",
+            text: "Edit GCP",
+            toggleGroup: toggleGroup,
+            enableToggle: true,
+            allowDepress: true,
+            control: this.modifyControl,
             deactivateOnDisable: true,
             map: this.target.mapPanel.map
         })]);
