d22374aca95086141d35e1d4ce1d0a06c315a556
Bart van den Eijnden <bartvde@opengeo.org>
2011-04-13 13:13:05 +0200

    add deleting of GCPs, more work on managing GCPs

--- app/static/script/app/AsBuilt/GCP.js
+++ app/static/script/app/AsBuilt/GCP.js
@@ -1,9 +1,82 @@
 Ext.namespace("AsBuilt.plugins");
 
+/**
+ * Class: OpenLayers.Control.DeleteFeature
+ * Deletes vector features from a given layer on click. 
+ *
+ * Inherits from:
+ *  - <OpenLayers.Control>
+ */
+OpenLayers.Control.DeleteFeature = OpenLayers.Class(OpenLayers.Control, {
+
+    /**
+     * Property: layer
+     * {<OpenLayers.Layer.Vector>}
+     */
+    layer: null,
+    
+    /**
+     * APIProperty: callbacks
+     * {Object} The functions that are sent to the handler for callback
+     */
+    callbacks: null,
+    
+    /**
+     * Property: handler
+     * {<OpenLayers.Handler.Feature>}
+     */
+    handler: null,
+
+    /**
+     * Constructor: <OpenLayers.Control.DeleteFeature>
+     *
+     * Parameters:
+     * layer - {<OpenLayers.Layer.Vector>} 
+     * options - {Object} 
+     */
+    initialize: function(layer, options) {
+        OpenLayers.Control.prototype.initialize.apply(this, [options]);
+        this.layer = layer;
+        this.callbacks = OpenLayers.Util.extend({
+            click: this.clickFeature
+        }, this.callbacks);
+        this.handler = new OpenLayers.Handler.Feature(this, layer,
+                                                      this.callbacks);
+    },
+
+    /**
+     * Method: clickFeature
+     * Called on click in a feature
+     * Only responds if this.hover is false.
+     *
+     * Parameters:
+     * feature - {<OpenLayers.Feature.Vector>} 
+     */
+    clickFeature: function(feature) {
+        this.layer.destroyFeatures([feature]);
+    },
+
+    /** 
+     * Method: setMap
+     * Set the map property for the control. 
+     * 
+     * Parameters:
+     * map - {<OpenLayers.Map>} 
+     */
+    setMap: function(map) {
+        this.handler.setMap(map);
+        OpenLayers.Control.prototype.setMap.apply(this, arguments);
+    },
+
+    CLASS_NAME: "OpenLayers.Control.DeleteFeature"
+});
+
 AsBuilt.plugins.GCP = Ext.extend(gxp.plugins.Tool, {
 
     /** api: ptype = app_gcp */
     ptype: "app_gcp",
+
+    gcpManager: null,
 
     /** api: config[type]
      * ``Integer`` Coordinate system type to use, one of: 
@@ -20,7 +93,7 @@ AsBuilt.plugins.GCP = Ext.extend(gxp.plugins.Tool, {
              *
              *  Listener arguments:
              *  * tool - :class:`AsBuilt.plugins.GCP` this tool
-             *  * geometry - :class: `OpenLayers.Geometry` the geometry created
+             *  * feature - :class: `OpenLayers.Feature.Vector` the feature created
              */
             "beforegcpadded",
 
@@ -29,9 +102,18 @@ AsBuilt.plugins.GCP = Ext.extend(gxp.plugins.Tool, {
              *
              *  Listener arguments:
              *  * tool - :class:`AsBuilt.plugins.GCP` this tool
-             *  * geometry - :class: `OpenLayers.Geometry` the geometry created
+             *  * feature - :class: `OpenLayers.Feature.Vector` the feature created
              */
-            "gcpadded"
+            "gcpadded",
+
+            /** api: event[gcpremoved]
+             *  Fired when a (part of a) gcp has been removed.
+             *
+             *  Listener arguments:
+             *  * tool - :class:`AsBuilt.plugins.GCP` this tool
+             *  * feature - :class: `OpenLayers.Feature.Vector` the feature created
+             */
+            "gcpremoved"
         );
         AsBuilt.plugins.GCP.superclass.constructor.apply(this, arguments);
     },
@@ -39,10 +121,21 @@ AsBuilt.plugins.GCP = Ext.extend(gxp.plugins.Tool, {
     /** api: method[addActions]
      */
     addActions: function() {
+        this.on({
+            "beforegcpadded": this.gcpManager.handleBeforeGCPAdded,
+            "gcpadded": this.gcpManager.handleGCPAdded,
+            "gcpremoved": this.gcpManager.handleGCPRemoved
+        });
         this.layer = new OpenLayers.Layer.Vector();
-        this.layer.events.on({"sketchcomplete": function(evt) {
-            return this.fireEvent("beforegcpadded", this, evt.feature.geometry);
-        }, scope: this});
+        this.layer.events.on({
+            "sketchcomplete": function(evt) {
+                return this.fireEvent("beforegcpadded", this, evt.feature);
+            },
+            "featureremoved": function(evt) { 
+                this.fireEvent("gcpremoved", this, evt.feature);
+            },
+            scope: this
+        });
         this.drawControl = new OpenLayers.Control.DrawFeature(
             this.layer,    
             OpenLayers.Handler.Point, {
@@ -51,9 +144,8 @@ AsBuilt.plugins.GCP = Ext.extend(gxp.plugins.Tool, {
                         if (!this.layer.map) {
                             this.target.mapPanel.map.addLayer(this.layer);
                         }
-                        var geometry = evt.feature.geometry;
                         this.drawControl.deactivate();
-                        this.fireEvent("gcpadded", this, geometry);
+                        this.fireEvent("gcpadded", this, evt.feature);
                     },
                     scope: this
                 }
@@ -61,7 +153,10 @@ AsBuilt.plugins.GCP = Ext.extend(gxp.plugins.Tool, {
         );
         this.modifyControl = new OpenLayers.Control.ModifyFeature(
             this.layer
-        );    
+        );
+        this.deleteControl = new OpenLayers.Control.DeleteFeature(
+            this.layer
+        );
         var toggleGroup = this.toggleGroup || Ext.id();
         var actions = AsBuilt.plugins.GCP.superclass.addActions.call(this, [new GeoExt.Action({
             tooltip: "Draw GCP",
@@ -79,6 +174,15 @@ AsBuilt.plugins.GCP = Ext.extend(gxp.plugins.Tool, {
             enableToggle: true,
             allowDepress: true,
             control: this.modifyControl,
+            deactivateOnDisable: true,
+            map: this.target.mapPanel.map
+        }), new GeoExt.Action({
+            tooltip: "Delete GCP",
+            text: "Delete GCP",
+            toggleGroup: toggleGroup,
+            enableToggle: true,
+            allowDepress: true,
+            control: this.deleteControl,
             deactivateOnDisable: true,
             map: this.target.mapPanel.map
         })]);
--- app/static/script/app/AsBuilt/GCPManager.js
+++ app/static/script/app/AsBuilt/GCPManager.js
@@ -9,25 +9,49 @@ AsBuilt.GCPManager = function(){
     var lastType = null;
 
     return Ext.apply(new Ext.util.Observable, {
-        handleBeforeGCPAdded: function(tool, geometry) {
-            if (tool.type === lastType) {
+        handleBeforeGCPAdded: function(tool, feature) {
+            if (lastType !== null && tool.type === lastType) {
                  // TODO instruct the user that he should first digitize a point
                  // in the Source Map followed by a corresponding point in the
                  // Reference Map 
                  return false;
             }
         },
-        handleGCPAdded: function(tool, geometry) {
+        handleGCPAdded: function(tool, feature) {
             if (tool.type === AsBuilt.plugins.GCP.IMAGE_COORDS) {
-                gcp = {source: geometry};
+                gcp = {source: feature};
             } else {
-                gcp.target = geometry;
+                gcp.target = feature;
                 gcps.push(gcp);
             }
             lastType = tool.type;
         },
+        handleGCPRemoved: function(tool, feature) {
+            lastType = null;
+            for (var i=0, ii=gcps.length; i<ii; ++i) {
+                if (gcps[i].source === feature) {
+                    // automatically remove the corresponding target
+                    var target = gcps[i].target;
+                    var layer = target.layer;
+                    layer && layer.destroyFeatures([target]);
+                }
+                if (gcps[i].target === feature) {
+                    // automatically remove the corresponding source
+                    var source = gcps[i].source;
+                    var layer = source.layer;
+                    layer && layer.destroyFeatures([source]);
+                }
+            }
+        },
         getGCPs: function() {
-            return gcps;
+            var result = [];
+            for (var i=0, ii=gcps.length; i<ii; ++i) {
+                var gcp = gcps[i];
+                if (gcp.source.geometry !== null && gcp.target.geometry !== null) {
+                    result.push(gcp);
+                }
+            }
+            return result;
         }
     })
 }();
