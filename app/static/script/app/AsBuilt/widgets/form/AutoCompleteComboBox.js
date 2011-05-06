6af5bfd545805489d4c51ea79f5e52472de4a069
tschaub <tim.schaub@gmail.com>
2011-05-06 11:59:24 -0600

    Allow for custom sorting of the combo's store based on parts of field values (closes #1).

--- app/static/script/app/AsBuilt/widgets/form/AutoCompleteComboBox.js
+++ app/static/script/app/AsBuilt/widgets/form/AutoCompleteComboBox.js
@@ -49,6 +49,42 @@ AsBuilt.form.AutoCompleteComboBox = Ext.extend(Ext.form.ComboBox, {
 
     srsName: null,
 
+    /** api: config[customSortInfo]
+     *  ``Object``
+     *  Providing custom sort info allows sorting of a single field value by
+     *  multiple parts within that value.  For example, a value representing
+     *  a street address like "1234 Main Street" would make sense to sort first
+     *  by "Main Street" (case insensitive) and then by "1234" (as an integer).
+     *  The ``customSortInfo`` object must contain ``matcher`` and ``parts``
+     *  properties.
+     *
+     *  The ``matcher`` value will be used to create a regular expression (with 
+     *  ``new RegExp(matcher)``).  This regular expression is assumed to have 
+     *  grouping parentheses for each part of the value to be compared.
+     * 
+     *  The ``parts`` value must be an array with the same length as the number
+     *  of groups, or parts of the value to be compared.  Each item in the 
+     *  ``parts`` array may have an ``order`` property and a ``sortType``
+     *  property.  The optional ``order`` value determines precedence for a part
+     *  (e.g. part with order 0 will be compared before part with order 1).
+     *  The optional ``sortType`` value must be a string matching one of the
+     *  ``Ext.data.SortTypes`` methods (e.g. "asFloat").
+     *
+     *  Example custom sort info to match addresses like "123 Main St" first by
+     *  street name and then by number:
+     *
+     *  .. code-block:: javascript
+     *  
+     *      customSortInfo: {
+     *          matcher: "^(\\d+)\\s(.*)$",
+     *          parts: [
+     *              {order: 1, sortType: "asInt"},
+     *              {order: 0, sortType: "asUCString"}
+     *          ]
+     *      }
+     */
+    customSortInfo: null,
+
     /** private: method[initComponent]
      *  Override
      */
@@ -73,9 +109,80 @@ AsBuilt.form.AutoCompleteComboBox = Ext.extend(Ext.form.ComboBox, {
                 srsName: this.srsName,
                 propertyNames: propertyNames,
                 maxFeatures: this.maxFeatures
-            }), setParamsAsOptions: true})
+            }), setParamsAsOptions: true}),
+            sortInfo: this.customSortInfo && {
+                field: this.id,
+                direction: this.customSortInfo.direction
+            }
         });
+        if (this.customSortInfo) {
+            this.store.createSortFunction = this.createCustomSortFunction();
+        }
         return AsBuilt.form.AutoCompleteComboBox.superclass.initComponent.apply(this, arguments);
+    },
+    
+    /** private: method[createCustomSortFunction]
+     *  If a ``customSortInfo`` property is provided, this method will be called
+     *  to replace the store's ``createSortFunction`` method.
+     */
+    createCustomSortFunction: function() {
+
+        /** Example customSortInfo:
+         *
+         *  customSortInfo: {
+         *      matcher: "^(\\d+)\\s(.*)$",
+         *      parts: [
+         *          {order: 1, sortType: "asInt"},
+         *          {order: 0, sortType: "asUCString"}
+         *      ]
+         *  };
+         */
+        
+        var matchRE = new RegExp(this.customSortInfo.matcher);
+        var num = this.customSortInfo.parts.length;
+        var orderLookup = new Array(num);
+        var part;
+        for (var i=0; i<num; ++i) {
+            part = this.customSortInfo.parts[i];
+            orderLookup[i] = {
+                index: i,
+                sortType: Ext.data.SortTypes[part.sortType || "none"],
+                order: part.order || 0
+            };
+        }
+        orderLookup.sort(function(a, b) {
+            return a.order - b.order;
+        });
+
+        // this method is the replacement for store.createSortFunction
+        return function(field, direction) {
+            direction = direction || "ASC";
+            var directionModifier = direction.toUpperCase() == "DESC" ? -1 : 1;
+            
+            // this is our custom comparison function that uses the given regex
+            // to sort by parts of a single field value
+            return function(r1, r2) {
+                var d1 = r1.data[field];
+                var d2 = r2.data[field];
+                var matches1 = matchRE.exec(d1) || [];
+                var matches2 = matchRE.exec(d2) || [];
+                
+                // compare matched parts in order - stopping at first unequal match
+                var cmp, v1, v2, lookup;
+                for (var i=0, ii=orderLookup.length; i<ii; ++i) {
+                    lookup = orderLookup[i];
+                    v1 = lookup.sortType(matches1[lookup.index + 1] || d1);
+                    v2 = lookup.sortType(matches2[lookup.index + 1] || d2);
+                    cmp = (v1 > v2 ? 1 : (v1 < v2 ? -1 : 0));
+                    if (cmp) {
+                        break;
+                    }
+                }
+                // flip the sign if descending
+                return directionModifier * cmp;
+            };
+        };
+
     }
 
 });
