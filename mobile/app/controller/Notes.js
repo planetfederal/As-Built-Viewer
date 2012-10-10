09f46699fc64a3b176cc4ff726e14c778470f571
Bart van den Eijnden <bartvde@opengeo.org>
2012-10-10 13:40:56 +0200

    a bit of formatting and styling of the notes list (#ASBUILT-6)

--- mobile/app/controller/Notes.js
+++ mobile/app/controller/Notes.js
@@ -17,15 +17,28 @@ Ext.define('AsBuilt.controller.Notes', {
 
     showNotes: function() {
         if (!this.getList()) {
-            Ext.Viewport.add({
+            var list = Ext.create("Ext.dataview.List", {
                 xtype: 'list',
-                width: 200,
+                width: 400,
                 height: 200,
-                right: 10,
-                bottom: 10,
-                itemTpl: '<div>{NOTE}</div>',
+                itemTpl: new Ext.XTemplate('<div class="notesNote">{NOTE}</div><div class="notesAuthor">Added {TIMESTAMP:this.formatTS} by {AUTHOR}</div>', {
+                    formatTS: function(value) {
+                        if (value !== null) {
+                            return Ext.Date.format(Ext.Date.parse(value, 'c'), 'F j, Y, g:i a');                           
+                        } else {
+                            return "";
+                        }
+                    }
+                }),
                 store: Ext.getStore('Notes')
             });
+            list.showBy(this.getNotesButton());
+        } else {
+            if (this.getList().getHidden()) {
+                this.getList().show();
+            } else {
+                this.getList().hide();
+            }
         }
     }
 
