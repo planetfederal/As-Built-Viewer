a3ea30e3d99a218b35a2a2fd9bfb0413db981e82
Bart van den Eijnden <bartvde@opengeo.org>
2012-11-21 17:02:08 +0100

    start work on validating content in FeatureEditorForm

--- mobile/app/view/FeatureEditorForm.js
+++ mobile/app/view/FeatureEditorForm.js
@@ -6,9 +6,14 @@ Ext.define("AsBuilt.view.FeatureEditorForm",{
         'Ext.field.Number',
         'Ext.field.Checkbox',
         'Ext.field.DatePicker',
-        'Ext.field.Select'
+        'Ext.field.Select',
+        'Ext.data.Validations'
     ],
     config: {
+        /** @cfg {OpenLayers.Feature.Vector}
+         *  The feature being edited/displayed.
+         */
+        feature: null,
         /** @cfg {Object}
          *  An object with as keys the field names, which will provide the ability
          *  to override the xtype that is created by default based on the
@@ -21,14 +26,19 @@ Ext.define("AsBuilt.view.FeatureEditorForm",{
          *  the field order from this list will be used, and fields missing in the
          *  list will be excluded.
          */
-        fields: null,
+        formFields: null,
         /** @cfg {Array}
          *  Optional list of field names (case sensitive) that are to be
          *  excluded from the form.
          */
         excludeFields: null,
+        /** @cfg {Boolean}
+         *  Set to true to disable editing. Default is false.
+         */
+        readOnly: null,
         language: "en",
         schema: null,
+        model: null,
         regexes: {
             "text": new RegExp(
                 "^(text|string)$", "i"
@@ -45,17 +55,26 @@ Ext.define("AsBuilt.view.FeatureEditorForm",{
         }
     },
     initialize:function() {
+        Ext.applyIf(Ext.data.Validations, {
+            range: function(config, value) {
+                return value !== undefined && value >= config.minValue && value <= config.maxValue;
+            }
+        });
         var r = this.getRegexes(), fieldCfg, record;
         var name, fields = [], schema = this.getSchema();
-        if (this.getFields()) {
-            for (var i=0, ii=this.getFields().length; i<ii; ++i) {
-                name = this.getFields()[i];
+        var modelConfig = {
+            fields: [],
+            validations: []
+        };
+        if (this.getFormFields()) {
+            for (var i=0, ii=this.getFormFields().length; i<ii; ++i) {
+                name = this.getFormFields()[i];
                 var idx = schema.findExact('name', name);
                 if (idx !== -1) {
                     record = schema.getAt(idx);
-                    fieldCfg = this.recordToField(record);
+                    fieldCfg = this.recordToField(record, modelConfig);
                     if (fieldCfg !== null) {
-                       fields.push(fieldCfg);
+                        fields.push(fieldCfg);
                     }
                 }
             }
@@ -67,25 +86,44 @@ Ext.define("AsBuilt.view.FeatureEditorForm",{
                     // exclude gml geometries
                     return;
                 }
-                var fieldCfg = this.recordToField(record);
+                var fieldCfg = this.recordToField(record, modelConfig);
                 if (fieldCfg !== null) {
                     fields.push(fieldCfg);
                 }
             }, this);
         }
+        this.modelId = Ext.id(null, 'gxm-attribute-model');
+        var model = Ext.define(this.modelId, {
+            extend: 'Ext.data.Model',
+            config: modelConfig
+        });
+        this.setModel(model);
         this.add(fields);
         this.callParent();
     },
 
-    recordToField: function(record, options) {
-        options = options || {};
+    validate: function() {
+        var instance = Ext.create(this.modelId, this.getValues());
+        var errors = instance.validate();
+    },
+
+    recordToField: function(record, modelConfig) {
         var name = record.get('name');
         if (this.getExcludeFields() && this.getExcludeFields().indexOf(name) !== -1) {
             return null;
         }
+        var options = {};
+        if (this.getReadOnly() === true) {
+            options.readOnly = true;
+        }
+        var feature = this.getFeature();
+        if (feature && feature.attributes[name] !== undefined) {
+            options.value = feature.attributes[name];
+        }
         var r = this.getRegexes();
         var type = record.get("type");
         type = type.split(":").pop();
+        modelConfig.fields.push({name: name, type: type});
         var fieldConfig = null;
         options.name = name;
         var restriction = record.get("restriction") || {};
@@ -117,12 +155,32 @@ Ext.define("AsBuilt.view.FeatureEditorForm",{
                 options: store
             }, options);
         } else if (type.match(r["text"])) {
+            var minLength = restriction["minLength"] !== undefined ?
+                parseFloat(restriction["minLength"]) : undefined;
+            var maxLength = restriction["maxLength"] !== undefined ?
+                parseFloat(restriction["maxLength"]) : undefined;
             fieldConfig = Ext.apply({
-                xtype: 'textfield'
+                xtype: 'textfield',
+                maxLength: maxLength
             }, options);
         } else if (type.match(r["number"])) {
+            var maxValue = restriction["maxInclusive"] !== undefined ?
+                parseFloat(restriction["maxInclusive"]) : undefined;
+            var minValue = restriction["minInclusive"] !== undefined ?
+                parseFloat(restriction["minInclusive"]) : undefined;
+            if (maxValue !== undefined || minValue !== undefined) {
+                modelConfig.validations.push({
+                    field: name,
+                    type: 'range',
+                    message: 'out of range, min: ' + minValue + ', max: ' + maxValue,
+                    minValue: minValue,
+                    maxValue: maxValue
+                });
+            }
             fieldConfig = Ext.apply({
-                xtype: 'numberfield'
+                xtype: 'numberfield',
+                minValue: minValue,
+                maxValue: maxValue
             }, options);
         } else if (type.match(r["boolean"])) {
             fieldConfig = Ext.apply({
